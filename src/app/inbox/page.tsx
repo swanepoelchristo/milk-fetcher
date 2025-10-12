f56720eee95da8d10ef5596e5401fa809964caff
Christo <217704679+swanepoelchristo@users.noreply.github.com>
2025-10-12 12:22:39 +0200

    Create page.tsx

---
+++ src/app/inbox/page.tsx
@@ -0,0 +1,5 @@
+// if your tsconfig has "@/..." alias (most Next apps do)
+import MoveForm from "@/components/MoveForm";
+
+// if that fails, use a relative import instead:
+// import MoveForm from "../../components/MoveForm";
